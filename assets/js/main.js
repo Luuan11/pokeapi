12b9139582eb3d026757451320c0977afca72710
Luan Fernando <luancraft55@gmail.com>
2022-11-18 20:31:38 -0300

    :sparkles: projeto feito

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,60 @@ const pokeName = document.querySelector('.poke-name');
 const pokeNumber = document.querySelector('.poke-number');
 const pokeGif = document.querySelector('.section-pokemon');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-search');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
 
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
 
+    if(APIResponse.status === 200){
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+
+    pokeName.innerHTML = 'Carregando...';
+
     const data = await fetchPokemon(pokemon);
 
-    pokeName.innerHTML = data.name;
-    pokeNumber.innerHTML = data.id;
-    pokeGif.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if (data) {
+        pokeGif.style.display = 'block';
+        pokeName.innerHTML = data.name;
+        pokeNumber.innerHTML = data.id;
+        pokeGif.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        searchPokemon = data.id;
+    
+        input.value = '';
+    } else{
+        pokeGif.style.display = 'none';
+        pokeName.innerHTML = 'Não encontrado :c';
+        pokeNumber.innerHTML = '';
+    }
+
 }
-renderPokemon('150')
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value.toLowerCase());
+});
+
+btnPrev.addEventListener('click', () => {
+    if(searchPokemon > 1){
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+btnNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
